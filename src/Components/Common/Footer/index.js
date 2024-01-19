46a7ecaece75d9e015111e95650dc2e37f4306a6
manan_joshi <joshimanan72@gmail.com>
2024-01-19 22:56:08 +0530

    changes

---
+++ src/Components/Common/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>footer</div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Common/Header/drawer.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { useState } from 'react';
+import Drawer from '@mui/material/Drawer';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import { IconButton } from '@mui/material';
+
+export default function TemporaryDrawer() {
+    
+    const [open, setOpen] = useState(false);
+    return (
+        <div>
+            <IconButton onClick={()=> setOpen(true)}><MenuRoundedIcon/></IconButton>
+            <Drawer anchor={"right"} open={setOpen(true)}
+                onClose={()=> setOpen(false)}
+            >
+            </Drawer>
+        <div className='drawer-div'>
+        <a href='./'>
+          <p className='link'>Home</p>
+          </a>
+          <a href='./'>
+          <p className='link'>Compare</p>
+          </a>
+          <a href='./'>
+          <p className='link'>Watchlist</p>
+          </a>
+          <a href='./'>
+          <p className='link'>Dashboard</p>
+          </a>
+
+      </div>
+            <h1>Hii</h1>
+            
+       </div>
+    );
+}
