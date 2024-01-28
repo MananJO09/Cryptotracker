caa6352a1ef4e4d6bc06a2d238ce34be6fd055d5
manan_joshi <joshimanan72@gmail.com>
2024-01-28 17:22:34 +0530

    new

--- src/Components/Dashboard/Tabs/index.js
+++ src/Components/Dashboard/Tabs/index.js
@@ -4,16 +4,16 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 
-export default function TabsComponent() {
+export default function TabsComponent({ coins }) {
     const [value, setValue] = useState('1');
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
 
-    const style ={
+    const style = {
         color: "var(--white)",
-        fontSize:"1.2rem",
+        fontSize: "1.2rem",
         fontWeight: "bold",
         textTransform: "capitalize",
     };
@@ -21,11 +21,33 @@ export default function TabsComponent() {
     return (
         <TabContext value={value}>
             <TabList onChange={handleChange} variant='fullWidth'>
-                <Tab label="Grid" value="1" sx={style}/>
-                <Tab label="List" value="2" sx={style}/>
+                <Tab label="Grid" value="1" sx={style} />
+                <Tab label="List" value="2" sx={style} />
             </TabList>
-            <TabPanel value="grid">Item One</TabPanel>
-            <TabPanel value="list">Item Two</TabPanel>
+            <TabPanel value="grid">
+                <div>
+                    {coins.map((item, i) => {
+                        return (
+                            <div key={i}>
+                                <img src={item.image} alt='' />
+                                <p>{i + 1} {item.name}
+                                </p>
+                            </div>
+                        );
+                    })}
+                </div>
+            </TabPanel>
+            <TabPanel value="list">
+                <div>
+                    {coins.map((item, i) => {
+                        return (
+                            <p key={i}>
+                                {i + 1}.{item.id}
+                            </p>
+                        );
+                    })}
+                </div>
+            </TabPanel>
         </TabContext>
     );
 }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../Components/Common/Header'
 import TabsComponent from '../Components/Dashboard/Tabs'
+import axios from 'axios';
 
 function DashboardPage() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+  //   fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en')
+  // .then((response) => {
+  //   // Code for handling the response
+  // })
+  // .catch((error) => {
+  //   // Code for handling the error
+  // });
+
+    axios
+    .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en'
+    )
+    .then((response) => {
+      console.log("RESPONSE>>>", response);
+      setCoins(response.data);
+    })
+    .catch((err )=> {
+       console.log("ERROR>>>", err);
+  });
+ }, []);
+
   return (
     <div>
-      <Header/>
-      <TabsComponent/>
+      <Header />
+      <TabsComponent coins={coins} />
     </div>
   )
 }
