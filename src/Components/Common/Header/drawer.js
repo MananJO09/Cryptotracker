f5772f331a5f54752f64ae3b30609db2130c571e
manan_joshi <joshimanan72@gmail.com>
2024-02-01 21:01:49 +0530

    new

--- src/Components/Common/Header/drawer.js
+++ src/Components/Common/Header/drawer.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { useState } from 'react';
 import Drawer from '@mui/material/Drawer';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
